b31c3d9a6a33c9d0b01e7e77f7d4b6ef908fa8f0
btk467@gmail.com <btk467@gmail.com>
2023-03-05 14:50:58 -0600

    connected Sentry

--- src/main/java/org/btk467/cnjokes/ChuckNorrisApplication.java
+++ src/main/java/org/btk467/cnjokes/ChuckNorrisApplication.java
@@ -2,12 +2,23 @@ package org.btk467.cnjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.sentry.Sentry;
 
 @SpringBootApplication
 public class ChuckNorrisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChuckNorrisApplication.class, args);
+
+	  Sentry.init(options -> {
+	    options.setDsn("https://af90203206ae4026b73f7f8d92106837@o335612.ingest.sentry.io/1866341");
+	    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+	    // We recommend adjusting this value in production.
+	    options.setTracesSampleRate(1.0);
+	    // When first trying Sentry it's good to see what the SDK is doing:
+	    options.setDebug(true);
+	  });
+
+	  SpringApplication.run(ChuckNorrisApplication.class, args);
 	}
 
 }
--- src/main/java/org/btk467/cnjokes/controller/ChuckNorrisController.java
+++ src/main/java/org/btk467/cnjokes/controller/ChuckNorrisController.java
@@ -6,6 +6,7 @@ package org.btk467.cnjokes.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -33,6 +34,12 @@ public class ChuckNorrisController {
 //    headers.add("user-agent", "Application");
 //    HttpEntity<String> entity = new HttpEntity<>(headers);
 
+    try {
+      throw new Exception("This is a test.");
+    } catch (Exception e) {
+      Sentry.captureException(e);
+    }
+    
     log.info("endpoint " + RANDOM_JOKE_URL );
     model.addAttribute("cn-random-joke-url", RANDOM_JOKE_URL);
     return "cn-random-joke";
