978f5665ecc22cedbfae19dc6240ba1ac91db938
btk467@gmail.com <btk467@gmail.com>
2023-04-09 14:33:13 -0500

    pushing the code which must fail in sonar

--- src/main/java/org/btk467/cnjokes/controller/ChuckNorrisController.java
+++ src/main/java/org/btk467/cnjokes/controller/ChuckNorrisController.java
@@ -3,6 +3,9 @@
  */
 package org.btk467.cnjokes.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,32 +20,39 @@ public class ChuckNorrisController {
 
   private static final String RANDOM_JOKE_URL = "https://api.chucknorris.io/jokes/random";
 
-  //private final ChuckNorrisService service;
+  // private final ChuckNorrisService service;
 
-//  public ChuckNorrisController(ChuckNorrisService service) {
-//    this.service = service;
-//  }
+  // public ChuckNorrisController(ChuckNorrisService service) {
+  // this.service = service;
+  // }
 
-  @GetMapping(value = {"/","/randomJoke"})
+  @GetMapping(value = {"/", "/randomJoke"})
   public String getRandomJoke(Model model) {
-    
-//    
-//    String randomJoke = service.getRandomJoke();
-//
-//    HttpHeaders headers = new HttpHeaders();
-//    headers.add("user-agent", "Application");
-//    HttpEntity<String> entity = new HttpEntity<>(headers);
 
-    log.info("endpoint " + RANDOM_JOKE_URL );
+    //
+    // String randomJoke = service.getRandomJoke();
+    //
+    // HttpHeaders headers = new HttpHeaders();
+    // headers.add("user-agent", "Application");
+    // HttpEntity<String> entity = new HttpEntity<>(headers);
+
+    log.info("endpoint " + RANDOM_JOKE_URL);
     model.addAttribute("cnRandomJokeUrl", RANDOM_JOKE_URL);
 
     return "cn-random-joke";
   }
-  
+
   @GetMapping(value = {"/crash"})
   public String crash() {
-    int x = 1 / 0;
+    List<String> strings = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+    Stream<String> pipeline = strings.stream().filter(b -> b.contains("2"));
+    long count1 = pipeline.count();
+    long count2 = pipeline.mapToLong(b -> b.length()).sum(); // Non-compliant;
+
+    System.out.println(String.format("Counts: %l %l", count1, count2));
+
     return "cn-random-joke";
   }
-  
+
 }
