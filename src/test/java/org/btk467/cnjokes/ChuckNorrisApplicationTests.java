09fa2d531e0f9ef1bcc086430dcf67b08f3399a9
btk467@gmail.com <btk467@gmail.com>
2023-04-09 13:51:22 -0500

    staged

--- src/test/java/org/btk467/cnjokes/ChuckNorrisApplicationTests.java
+++ src/test/java/org/btk467/cnjokes/ChuckNorrisApplicationTests.java
@@ -1,5 +1,8 @@
 package org.btk467.cnjokes;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,17 @@ class ChuckNorrisApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	//@Test
+	void testSonarCloudRule() {
+	  
+	  List<String> strings = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+	  
+	  Stream<String> pipeline = strings.stream().filter(b -> b.contains("2"));
+	      long count1 = pipeline.count();
+	      long count2 = pipeline.mapToLong(b -> b.length()).sum(); // Non-compliant;
+	      System.out.println(String.format("Counts: %l %l",count1,count2));
 	}
 
 }
